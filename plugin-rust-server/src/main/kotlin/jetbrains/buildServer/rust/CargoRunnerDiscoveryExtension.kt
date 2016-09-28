a439e30dcaa93b2e1aff7cf4a44f4d8c79dbde94
Dmitry.Tretyakov <dtretyakov@gmail.com>
2016-09-28 13:23:26 +0300

    Add auto-discovery of build steps
    Issue: #4

---
+++ plugin-rust-server/src/main/kotlin/jetbrains/buildServer/rust/CargoRunnerDiscoveryExtension.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * See LICENSE in the project root for license information.
+ */
+
+package jetbrains.buildServer.rust
+
+import jetbrains.buildServer.serverSide.discovery.BreadthFirstRunnerDiscoveryExtension
+import jetbrains.buildServer.serverSide.discovery.DiscoveredObject
+import jetbrains.buildServer.util.browser.Element
+import java.util.*
+
+/**
+ * Performs cargo build steps discovery.
+ */
+class CargoRunnerDiscoveryExtension : BreadthFirstRunnerDiscoveryExtension(1) {
+    override fun discoverRunnersInDirectory(dir: Element, filesAndDirs: MutableList<Element>): MutableList<DiscoveredObject> {
+        val result = ArrayList<DiscoveredObject>()
+        for (item in filesAndDirs) {
+            if (item.isLeaf && item.name == CargoConstants.PROJECT_FILE) {
+                result.add(DiscoveredObject(CargoConstants.RUNNER_TYPE,
+                        mapOf(Pair(CargoConstants.PARAM_COMMAND, CargoConstants.COMMAND_BUILD))))
+                result.add(DiscoveredObject(CargoConstants.RUNNER_TYPE,
+                        mapOf(Pair(CargoConstants.PARAM_COMMAND, CargoConstants.COMMAND_TEST))))
+            }
+        }
+
+        return result
+    }
+}
