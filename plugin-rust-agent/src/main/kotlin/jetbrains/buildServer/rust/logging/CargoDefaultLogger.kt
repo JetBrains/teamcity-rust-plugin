86715d8e54ca69e33ca5ef65a66807032df25ab1
Dmitry.Tretyakov <dtretyakov@gmail.com>
2016-09-28 12:59:16 +0300

    Improve failed tests reporting

--- plugin-rust-agent/src/main/kotlin/jetbrains/buildServer/rust/logging/CargoDefaultLogger.kt
+++ plugin-rust-agent/src/main/kotlin/jetbrains/buildServer/rust/logging/CargoDefaultLogger.kt
@@ -8,6 +8,7 @@
 package jetbrains.buildServer.rust.logging
 
 import jetbrains.buildServer.agent.BuildProgressLogger
+import java.util.regex.Pattern
 
 /**
  * Default logger implementation.
@@ -31,5 +32,32 @@ open class CargoDefaultLogger(private val myLogger: BuildProgressLogger) : Cargo
 
     override fun canChangeState(state: CargoState, text: String): Boolean {
         return true
+    }
+
+    companion object {
+        private val tokens = mapOf(
+                Pair("'", "|'"),
+                Pair("\n", "|n"),
+                Pair("\r", "|r"),
+                Pair("|", "||"),
+                Pair("[", "|["),
+                Pair("]", "|]"))
+
+        private val ESCAPE_PATTERN = Pattern.compile(tokens.keys.joinToString(
+                separator = "|",
+                prefix = "(",
+                postfix = ")",
+                transform = { Pattern.quote(it) }))
+
+        fun escapeValue(text: String): String {
+            val matcher = ESCAPE_PATTERN.matcher(text)
+
+            val sb = StringBuffer()
+            while (matcher.find()) {
+                matcher.appendReplacement(sb, tokens[matcher.group(1)])
+            }
+
+            return matcher.appendTail(sb).toString()
+        }
     }
 }
