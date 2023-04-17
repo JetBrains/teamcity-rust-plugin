595561db6600475e4f3933006ed5924acf356376
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-04-17 09:19:39 +0200

    #60 installing custom crates in the `--locked` mode

--- plugin-rust-agent/src/main/kotlin/jetbrains/buildServer/rust/CargoInstallCrateService.kt
+++ plugin-rust-agent/src/main/kotlin/jetbrains/buildServer/rust/CargoInstallCrateService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2021 JetBrains s.r.o.
+ * Copyright 2000-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * See LICENSE in the project root for license information.
@@ -11,16 +11,14 @@ import jetbrains.buildServer.RunBuildException
 import jetbrains.buildServer.agent.ToolCannotBeFoundException
 import jetbrains.buildServer.agent.runner.BuildServiceAdapter
 import jetbrains.buildServer.agent.runner.ProcessListener
-import jetbrains.buildServer.agent.runner.ProcessListenerAdapter
 import jetbrains.buildServer.agent.runner.ProgramCommandLine
-import jetbrains.buildServer.rust.logging.BlockListener
 
 class CargoInstallCrateService(private val crate: String) : BuildServiceAdapter() {
 
     val errors = arrayListOf<String>()
     override fun makeProgramCommandLine(): ProgramCommandLine {
         val cargoPath = getCargoPath()
-        return createProgramCommandline(cargoPath, arrayListOf("install", crate))
+        return createProgramCommandline(cargoPath, arrayListOf("install", "--locked", crate))
     }
 
     private fun getCargoPath(): String {
