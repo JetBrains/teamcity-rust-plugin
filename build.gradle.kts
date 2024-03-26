03ba67731c6634f50b198e4e8c103dab3dcd6cb9
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-03-26 14:08:29 +0100

    housekeeping
    - upgrading build dependencies
    - adding missing `.gitignore` records
    - replacing deprecated stdlib calls
    - typos

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,25 @@
-
-
 import java.text.SimpleDateFormat
 import java.util.*
 
-buildscript {
-    repositories {
-        mavenCentral()
-    }
+repositories {
+    mavenCentral()
+}
 
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-    }
+plugins {
+    kotlin("jvm") version "1.9.23"
 }
 
 val groupId = "teamcity-rust-plugin"
 
-val pluginVersionDateFormat = SimpleDateFormat("yyyyMMddHHmmss")
-val correctVersion = project.hasProperty("versionNumber") &&
-        Regex("""\d+\.\d+\.\d+.*""").matches(property("versionNumber") as String)
-val versionNumber = if (correctVersion) {
-    property("versionNumber")!!
-} else {
-    "SNAPSHOT-" + pluginVersionDateFormat.format(Date())
-}
-
-extra["teamcityVersion"] = if (project.hasProperty("teamcityVersion")) {
-    property("teamcityVersion")
-} else {
-    "2020.2"
-}
+extra["teamcityVersion"] = project.findProperty("teamcityVersion") ?: "2020.2"
 
 extra["downloadsDir"] = project.findProperty("downloads.dir") ?: "$rootDir/downloads"
 extra["serversDir"] = project.findProperty("servers.dir") ?: "$rootDir/servers"
 
 group = groupId
-version = versionNumber
+version = project.findProperty("versionNumber")
+    ?.takeIf { Regex("""\d+\.\d+\.\d+.*""").matches(it as String) }
+    ?: "SNAPSHOT-" + SimpleDateFormat("yyyyMMddHHmmss").format(Date())
 
 allprojects {
     group = groupId
